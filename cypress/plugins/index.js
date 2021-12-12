ec6364218eac571775eebf0e516ceb718cbd7a8b
Poliana Rufatto <poliana.rufatto@softplan.com.br>
2021-12-12 15:24:12 +0100

    feat: Add allure report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 /**
  * @type {Cypress.PluginConfig}
@@ -8,5 +9,6 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber());
+  allureWriter(on, config);
   return config;
 };
--- cypress/support/pages/index.js
+++ cypress/support/pages/index.js
@@ -3,4 +3,3 @@ export * from './menu.page';
 export * from './robots.page';
 export * from './seo.page';
 export * from './url.page';
-
